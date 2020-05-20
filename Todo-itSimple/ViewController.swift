cca7e8c7e93008a0671b86c37ef382ed0f7cac89
Lucas van Leerdam <lukienl@gmail.com>
2020-05-20 14:12:01 +0200

    Initial Commit

---
+++ Todo-itSimple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo-itSimple
+//
+//  Created by Lucas van Leerdam on 20/05/2020.
+//  Copyright © 2020 Lucas van Leerdam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
